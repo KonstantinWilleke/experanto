1a6fc8bf9005def69208cb6b09944b695557e6aa
konstantinwilleke <konstantin.willeke@gmail.com>
2025-12-17 11:45:48 -0800

    Extend default configs in configs.py

--- experanto/configs.py
+++ experanto/configs.py
@@ -12,3 +12,11 @@ DEFAULT_CONFIG = cfg
 DEFAULT_DATASET_CONFIG = cfg.dataset
 DEFAULT_MODALITY_CONFIG = cfg.dataset.modality_config
 DEFAULT_DATALOADER_CONFIG = cfg.dataloader
+
+# get config relative to this file
+script_dir = Path(__file__).parent
+config_path = script_dir / '..' / 'configs' / 'throughput_f16_prenorm.yaml'
+config_path = config_path.resolve()
+cfg = OmegaConf.load(config_path)
+
+BENCHMARKING_CONFIG = cfg
